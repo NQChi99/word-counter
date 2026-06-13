c0b21cc3283010c3e959b8b1c9e75a9dcf53af23
Chi Nguyen <tamara.chi@seznam.cz>
2026-06-13 16:56:34 +0200

    added text tools (paste, copy, clear)

--- script.js
+++ script.js
@@ -6,6 +6,14 @@ const numberParagraphs = document.getElementById("paragraphs-number");
 
 const textInput = document.getElementById("textInput");
 
+const copyBtn = document.getElementById("copy");
+const pasteBtn = document.getElementById("paste");
+const clearBtn = document.getElementById("clear");
+
+const pastedTooltip = document.getElementById("tooltip-pasted");
+const copiedTooltip = document.getElementById("tooltip-copied");
+const clearedTooltip = document.getElementById("tooltip-cleared");
+
 // funkce na počet znaků
 const countCharacters = () => {
     let str = textInput.value;
@@ -40,9 +48,47 @@ const countParagraphs = () => {
     return str.split(/\n\s*\n|\n+/).filter(p => p.trim().length > 0).length;
 }
 
-textInput.addEventListener("input", () => {
+// funkce na všechna počítadla
+const countAll = () => {
     numberWords.textContent = countWords();
     numberCharacters.textContent = countCharacters();
     numberSentences.textContent = countSentences();
     numberParagraphs.textContent = countParagraphs();
+}
+
+textInput.addEventListener("input", () => {
+    countAll();
+})
+
+copyBtn.addEventListener("click", () => {
+    navigator.clipboard.writeText(textInput.value);
+
+    copiedTooltip.style.opacity = "1";
+    setTimeout(() => {
+        copiedTooltip.style.opacity = "0";
+    }, 2000);
+})
+
+pasteBtn.addEventListener("click", () => {
+    navigator.clipboard
+        .readText()
+        .then((clipText) => {
+            textInput.value = clipText;
+            countAll();
+        });
+
+    pastedTooltip.style.opacity = "1";
+    setTimeout(() => {
+        pastedTooltip.style.opacity = "0";
+    }, 2000);
+})
+
+clearBtn.addEventListener("click", () => {
+    textInput.value = "";
+    countAll();
+
+    clearedTooltip.style.opacity = "1";
+    setTimeout(() => {
+        clearedTooltip.style.opacity = "0";
+    }, 2000)
 })
